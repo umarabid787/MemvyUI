ac6f8b8e5a19cd3cb15a11ad92a1213e176a7d2c
salahuddin1005 <81920392+salahuddin1005@users.noreply.github.com>
2024-11-07 13:37:43 +0500

    Pop-up Updated

--- my-material-app/src/components/MediaModal.js
+++ my-material-app/src/components/MediaModal.js
@@ -1,23 +1,39 @@
 import React from 'react';
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography, Box } from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography } from '@mui/material';
+import { styled } from '@mui/system';
+
+const StyledDialogContent = styled(DialogContent)({
+  backgroundColor: '#2B3672',
+  color: 'white',
+});
+
+const StyledDialogTitle = styled(DialogTitle)({
+  backgroundColor: '#2B3672',
+  color: 'white',
+});
+
+const StyledDialogActions = styled(DialogActions)({
+  backgroundColor: '#2B3672',
+});
 
 const MediaModal = ({ open, onClose, mediaItem }) => {
   if (!mediaItem) return null;
 
   return (
-    <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
-      <DialogTitle>{mediaItem.username}</DialogTitle>
-      <DialogContent>
+    <Dialog open={open} onClose={onClose} maxWidth="md">
+      {/* <StyledDialogTitle>{mediaItem.username}</StyledDialogTitle>
+      <StyledDialogTitle>{mediaItem.date}</StyledDialogTitle> */}
+      <StyledDialogContent>
         {mediaItem.type === 'image' && (
           <img src={mediaItem.src} alt={mediaItem.alt} style={{ width: '100%' }} />
         )}
         {mediaItem.type === 'audio' && <audio controls src={mediaItem.src} />}
         {mediaItem.type === 'video' && <video controls src={mediaItem.src} />}
         {mediaItem.type === 'text' && <Typography>{mediaItem.content}</Typography>}
-      </DialogContent>
-      <DialogActions>
+      </StyledDialogContent>
+      <StyledDialogActions>
         <Button onClick={onClose} color="primary">Close</Button>
-      </DialogActions>
+      </StyledDialogActions>
     </Dialog>
   );
 };
